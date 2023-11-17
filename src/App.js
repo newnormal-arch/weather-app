6aa299f9265f2a612a0548b781e3777fe9cc153c
Bongz Podolski <ntshingilab@outlook.com>
2023-11-17 14:21:52 +0200

    basic ui changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=0214c3ef3160f620bc044be3b68270fe';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main-container'>
+        <div className='top'>
+          <div className='location-details'>
+            <p>Johannesburg</p>
+          </div>
+
+          <div className='temp'>
+            <p>23 *C</p>
+          </div>
+
+          <div className='description'>
+            <p>Cloudy</p>
+          </div>
+
+        </div>
+        <div className='bottom'>
+          <div className='temp-details'>
+            <p>23 *C</p>
+          </div>
+          <div className='humidity'>
+            <p>20 %</p>
+          </div>
+          <div className='winds'>
+            <p>6 k/ph</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
