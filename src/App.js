af6828647b4c53d77f1bf1ae4bc7e287236a25ef
Bongz Podolski <ntshingilab@outlook.com>
2023-11-17 15:58:00 +0200

    api added, just need error handling to sort out

--- src/App.js
+++ src/App.js
@@ -3,37 +3,63 @@ import axios from 'axios';
 
 
 function App() {
+  const API_KEY = '0214c3ef3160f620bc044be3b68270fe';
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
 
-  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=0214c3ef3160f620bc044be3b68270fe';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {      
+        axios.get(url).then((response) => {
+          setData(response.data)
+          console.log(response.data)
+        })
+        setLocation('')      
+    }
+  }
 
   return (
     <div className="App">
+      <div className='search'>
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type='text'/>
+      </div>
       <div className='main-container'>
         <div className='top'>
           <div className='location-details'>
-            <p>Johannesburg</p>
+            <p>{data.name}</p>
           </div>
 
           <div className='temp'>
-            <p>23 *C</p>
+          {data.main ? <p>{Math.round(data.main.temp)}°C</p> : null}
           </div>
 
           <div className='description'>
-            <p>Cloudy</p>
+            <p>{data.weather ? <p>{data.weather[0].main}</p> : null}</p>
           </div>
 
         </div>
-        <div className='bottom'>
-          <div className='temp-details'>
-            <p>23 *C</p>
+        {data.name !== undefined &&
+          <div className='bottom'>
+          <div className='temp-min'>
+          {data.main ? <p>{Math.round(data.main.temp_min)} °C</p> : null}
+            <p>Min</p>
+          </div>
+          <div className='temp-max'>
+          {data.main ? <p>{Math.round(data.main.temp_max)} °C</p> : null}
+            <p>Max</p>
           </div>
           <div className='humidity'>
-            <p>20 %</p>
-          </div>
-          <div className='winds'>
-            <p>6 k/ph</p>
+          {data.main ? <p>{Math.round(data.main.humidity)} %</p> : null}
+            <p>Humidity</p>
           </div>
         </div>
+        }
       </div>
     </div>
   );
